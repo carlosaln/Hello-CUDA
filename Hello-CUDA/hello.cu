96d28c98ab30bff8769fad06eb6f9751ebade8f1
Carlos Lemus <cal0018@auburn.edu>
2014-10-04 12:38:16 -0500

    first commit

---
+++ Hello-CUDA/hello.cu
@@ -0,0 +1,35 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ static void sayHello()
+{
+    int i = blockIdx.x*blockDim.x+threadIdx.x;
+
+    printf("Hello from thread %d!\n", i);
+}
+
+int main()
+{
+    int grid_size;
+    int tpb;
+
+    puts("Please enter the grid size: ");
+    scanf("%d", &grid_size);
+
+    puts("\nPlease enter the threads per block: ");
+    scanf("%d", &tpb);
+
+    puts("\n");
+
+    sayHello<<<grid_size,tpb>>>();
+
+    cudaError_t cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
